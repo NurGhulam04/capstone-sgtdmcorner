9378b20afecb6d5fb38399929b5de51579eb60c2
Logmaster12 <rafiaqila11@gmail.com>
2025-11-07 12:14:38 +0700

    dashboard, pendahuluan, dan video

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,10 +17,48 @@ class DashboardController extends Controller
 
         return view('dashboard');
     }
-
     public function pendahuluan()
     {
+        // View ini hanya akan menampilkan konten statis.
         return view('pendahuluan');
     }
 
+
+    public function video()
+    {
+
+        $videos = [
+            [
+                'judul' => 'Pengetahuan tentang DM',
+                'url' => 'https://youtu.be/xkqmyoBYE9M?si=yVa3Xrx9qdGWcCiT'
+            ],
+            [
+                'judul' => 'Spiritual Group Therapy',
+                'url' => 'https://youtu.be/CdTIici0-7w?si=dQDUrAyrhCeaXGkJ'
+            ],
+            [
+                'judul' => 'Ergonomic Exercise based Spiritual Care',
+                'url' => 'https://youtu.be/MlM7C5KVV2Y?si=wbF85xK4HaL3xiij'
+            ],
+            [
+                'judul' => 'Buerger Allen Exercise',
+                'url' => 'https://youtu.be/dwP-sLQU0hM?si=tLv3gDjhtpBOhQfp'
+            ],
+            [
+                'judul' => 'Mindfulness Meditation based Spiritual Care',
+                'url' => 'https://youtu.be/TRSag-IfFg4?si=xBLUCOEfaNufoi1G'
+            ],
+            [
+                'judul' => 'Pemberian Aromatherapy',
+                'url' => 'https://youtu.be/DkUcwa4gdPE?si=37W2hfsbw-1dADAb'
+            ],
+            [
+                'judul' => 'Diabetic Foot Spa',
+                'url' => 'https://youtu.be/ph57saV2Hp4?si=DFKlv5YlDLx3uSwU'
+            ],
+        ];
+
+
+        return view('video', compact('videos'));
+    }
 }
